3b3383e29b4492260fd8d13de3f13262e1d43baa
Manikumar157 <devarinti2015@gmail.com>
2021-11-06 11:25:10 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringBootdocketTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootdocketTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootdocketTestApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
